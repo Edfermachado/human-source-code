36f8b63eedd53e4408bcb55430e8a71d2535c8a8
Edfermachado <edfermachado10@gmail.com>
2026-03-08 19:46:19 -0400

    fix: Corregir la ruta base en la configuración de Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   // Reemplaza <TU_USUARIO> con tu nombre de usuario real en GitHub
   site: 'https://edfermachado.github.io',
-  base: 'human-source-code',
+  base: '/human-source-code',
 });
